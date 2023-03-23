50007a810113e153e86fc5e75ef276c376bcbef0
Santosh Ganti <sagant@microsoft.com>
2023-03-22 18:51:55 -0700

    Windows Apps using Winget

---
+++ WingetApps/WingetApps.ps1
@@ -0,0 +1,91 @@
+  
+# First download AppInstaller App from Windows Store which install winget commandline tool
+# winget source add --name msstore https://storeedgefd.dsx.mp.microsoft.com/v9.0
+
+# Terminals
+winget install Microsoft.WindowsTerminal
+winget install Microsoft.WindowsTerminal.Preview
+winget install Microsoft.PowerShell
+
+# Git Tools
+winget install Microsoft.Git
+winget install GitHub.GitLFS
+winget install Microsoft.GitCredentialManagerCore
+winget install GitHub.GitHubDesktop
+
+# Command line utilities
+winget install Microsoft.Sqlcmd
+winget install Microsoft.AzureCLI
+winget install Microsoft.AzureDataCLI
+winget install Microsoft.PowerAppsCLI
+winget install CoreyButler.NVMforWindows
+
+# System Utilities
+winget install Microsoft.WindowsApplicationDriver
+winget install Microsoft.DirectX
+winget install Microsoft.MSIXCore
+winget install Microsoft.EdgeWebView2Runtime 
+
+# Development & Build Tools
+winget install Python.Python.3.12
+winget install Python.Python.2
+winget install Microsoft.NuGet
+winget install Microsoft.DotNet.SDK.7
+winget install Microsoft.DotNet.SDK.6 
+winget install Microsoft.DotNet.SDK.5 
+winget install Microsoft.DotNet.SDK.3_1
+winget install Microsoft.ServiceFabricRuntime
+winget install Microsoft.AzureFunctionsCoreTools
+winget install Microsoft.AzureStorageEmulator
+
+# Visual Studio Code
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.VisualStudioCode.Insiders
+winget install Microsoft.AzureMediaServicesExplorer 
+winget install Microsoft.AzureDataStudio
+
+# Visual Studio and build tools
+winget install Microsoft.VisualStudio.2022.Enterprise
+winget install Microsoft.VisualStudio.2022.Enterprise.Preview
+
+# Storage browsing tools
+winget install Microsoft.ServiceFabricExplorer
+winget install Microsoft.AzureStorageExplorer
+
+# Browsers
+winget install Microsoft.Edge
+winget install Google.Chrome
+winget install Mozilla.Firefox
+
+# Databases
+winget install Microsoft.SQLServerManagementStudio
+winget install Microsoft.SQLServer.2019.Developer
+
+# Necessary Applications
+winget install Adobe.Acrobat.Reader.64-bit
+winget install Microsoft Office
+winget install Microsoft.OneDrive
+winget install Microsoft.VisioViewer
+
+# Optional Utilities
+winget install Microsoft.dotnetUninstallTool
+
+# Optional Applications
+winget install Google.Drive
+
+# Apps
+winget install Microsoft.PowerBI
+winget install Microsoft.MouseandKeyboardCenter
+winget install Microsoft.Teams
+winget install Microsoft.PCManager 
+
+
+# Uncomment below lines to install WSL distributions of WSL
+# Please install WSL from Microsoft Store before running the commands
+
+# winget install Canonical.Ubuntu.2204
+# winget install Canonical.Ubuntu.2004
+# winget install Debian.Debian
+
+# Drivers
+winget install DisplayLink.GraphicsDriver
