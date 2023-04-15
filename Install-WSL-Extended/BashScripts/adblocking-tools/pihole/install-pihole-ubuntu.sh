f35c13dad09cd06492be6f9821fb4206b3f4556f
Santosh Ganti <santosh.ganti@live.com>
2023-04-14 18:51:38 -0700

    Adding pihole

---
+++ Install-WSL-Extended/BashScripts/adblocking-tools/pihole/install-pihole-ubuntu.sh
@@ -0,0 +1,13 @@
+# Pihole Documentation - https://docs.pi-hole.net/main/basic-install/
+curl -sSL https://install.pi-hole.net | bash
+
+# Alternative 1: Clone repository and run
+
+# git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
+# cd "Pi-hole/automated install/"
+# sudo bash basic-install.sh
+
+# Alternative 2: Manually download the installer and run
+
+# wget -O basic-install.sh https://install.pi-hole.net
+# sudo bash basic-install.sh
